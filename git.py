ea2e1e01522e0bf6ac9955f02f4a24db8d4072c7
Bishwas Purkoti <momoparty10@gmail.com>
2026-01-01 13:20:50 +0545

    git code cheatcode

--- git.py
+++ git.py
@@ -3,4 +3,12 @@
 # git checkout -b feature # the new branch is created named feature
 # git checkout main # switching to existing branch
 # git branch -r              # local branch & remote branch
-# git 
+# git branch -m <new-name>
+
+
+# git remote -v
+# git remote add origin https://github.com/purkotibp/Python_class.git
+# git remote -v                                                      
+# origin  https://github.com/purkotibp/Python_class.git (fetch)
+# origin  https://github.com/purkotibp/Python_class.git (push)
+# git push origin main
