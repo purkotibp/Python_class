aa211a1d7cce90e667eac3f95d8234f54b926561
Bishwas Purkoti <momoparty10@gmail.com>
2026-01-01 12:42:13 +0545

    update for testing

---
+++ git.py
@@ -0,0 +1,6 @@
+# git branch #to check branch
+# git checkout -branch # switching into new branch 
+# git checkout -b feature # the new branch is created named feature
+# git checkout main # switching to existing branch
+# git branch -r              # local branch & remote branch
+# git 
