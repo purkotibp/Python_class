93bb52866e05cc4b15428c2e97ca4bd13c5080cc
Bishwas Purkoti <momoparty10@gmail.com>
2026-01-13 10:02:01 +0545

    update with assignment of abstraction

---
+++ abstraction_bishwas_purkoti_task.py
@@ -0,0 +1,23 @@
+# class Text_App:
+# #     def __init__(self):
+# #         self.time=1
+
+# #     def text_msg(self):
+# #         print("the messaged recieved.")
+# #         self._message_sent()
+# #         self._message_deliverd()
+# #         self._message_seen()
+
+
+# #     def _message_sent(self):
+# #         print("Write a message")
+
+# #     def _message_deliverd(self):
+# #         print("the message is sent and deliverd")
+    
+# #     def _message_seen(self):
+# #         print("the message is seen")
+
+# # messenger=Text_App()
+# # messenger.text_msg()
+
